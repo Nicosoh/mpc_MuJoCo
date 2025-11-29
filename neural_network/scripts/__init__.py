9c22b9790c4570a4c0fb16839e35b402fe74e41c
Nicodemus <N.sohyuji@student.tudelft.nl>
2025-11-29 16:14:26 +0100

    Added NN file structure

---
+++ neural_network/scripts/__init__.py
@@ -0,0 +1,3 @@
+from neural_network.scripts.model import PendulumNetwork
+
+__all__ = ["PendulumNetwork"]
---
+++ neural_network/scripts/model.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class PendulumNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.fc1 = nn.Linear(2, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, 64)
+
+    def forward(self, x):
+        x = F.tanh(self.fc1(x))
+        x = F.tanh(self.fc2(x))
+        x = F.tanh(self.fc3(x))    # z ∈ R^64
+
+        # final output = 1/2 * ||z||^2
+        x = 0.5 * torch.sum(x**2, dim=1, keepdim=True)
+
+        return x
