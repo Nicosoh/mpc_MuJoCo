1eea4693f2c9928b55f05d476d89582a74c9c246
Nicodemus Soh <nicosohh@gmail.com>
2026-01-08 19:12:40 +0100

    updated IK to also include acceleration control.

--- IK_copy.py
+++ IK_copy.py
@@ -26,8 +26,11 @@ class InverseKinematicsSolver:
         self.max_iterations = self.config["IK"]["max_iterations"]
         self.stop_thres = self.config["IK"]["stop_threshold"]
         self.dt = config["mpc"]["mpc_timestep"]
+        self.velocity_thres = self.config["IK"]["velocity_threshold"]
+        self.limit_accel = self.config["IK"]["limit_acceleration"]
 
-        self.traj_qp = []
+        self.traj_q = []
+        self.traj_v = []
 
         self.setup()
 
@@ -44,12 +47,15 @@ class InverseKinematicsSolver:
                 orientation_cost=1e-3 # Pose of the end-effector
             ),
             "posture": PostureTask(cost=1e-2)} # Regularisation of angles
-
+        q = np.array([-1.0,0.0])
         # Initial configuration
-        self.configuration = pink.Configuration(model=self.robot.model, data=self.robot.data, q=self.robot.q0, collision_model=self.robot.collision_model, collision_data=self.robot.collision_data)
+        self.configuration = pink.Configuration(model=self.robot.model, data=self.robot.data, q=q, collision_model=self.robot.collision_model, collision_data=self.robot.collision_data)
 
         # Select QP solver
         self.solver = "daqp" if "daqp" in qpsolvers.available_solvers else qpsolvers.available_solvers[0]
+
+        if self.limit_accel:
+            self.limits = (self.configuration.model.velocity_limit, self.accellimit)
         
     def load_model(self):
         # Load model from XML
@@ -61,8 +67,9 @@ class InverseKinematicsSolver:
             # Load from MJCF
             self.robot = RobotWrapper.BuildFromMJCF(filename=filename)
             # Set velocitylimit 
-            self.robot.model.velocityLimit = np.array(self.config["model"]["velocitylimit"])
-            self.accellimit = AccelerationLimit(self.robot.model, np.array(self.config["model"]["velocitylimit"])*10)
+            self.robot.model.velocityLimit = np.array(self.config["IK"]["velocitylimit"])
+            if self.limit_accel:
+                self.accellimit = AccelerationLimit(self.robot.model, np.array(self.config["IK"]["accelerationlimit"]))
         except FileNotFoundError:
             raise FileNotFoundError(f"Model file '{filename}' does not exist. Check your models_xml folder.")
         
@@ -152,25 +159,26 @@ class InverseKinematicsSolver:
 
     def pad_yref(self):
         """Pad yref to match target_dim by adding zeros."""
-        yref_vel = np.zeros_like(self.traj_qp)
-        yref_u = np.zeros((self.traj_qp.shape[0], self.config["pin"]["nu"]))
+        yref_vel = np.zeros_like(self.traj_q)
+        yref_u = np.zeros((self.traj_q.shape[0], self.config["pin"]["nu"]))
 
-        self.yref_pos_vel = np.hstack([self.traj_qp, yref_vel])
-        self.yref_full = np.hstack([self.traj_qp, yref_vel, yref_u])
+        self.yref_pos_vel = np.hstack([self.traj_q, yref_vel])
+        self.yref_full = np.hstack([self.traj_q, yref_vel, yref_u])
     
     def IK_loop(self, record: bool, x0: bool):
         iterations = 0
+        vel_below_thresh = 0
         error = np.linalg.norm(self.T_target.translation - self.configuration.get_transform_frame_to_world(self.tasks["ee"].frame).translation)
-        limits = (self.configuration.model.velocity_limit, self.accellimit)
-        while error > self.stop_thres:
-            velocity = solve_ik(self.configuration, self.tasks.values(), self.dt, solver=self.solver, barriers=self.barriers, limits = limits, safety_break=False)
 
+        while error > self.stop_thres:
             # Integrate motion
-            # q_out = self.configuration.integrate(velocity, self.dt)
-            self.configuration.integrate_inplace(velocity, self.dt)
-            self.accellimit.set_last_integration(velocity, self.dt)
-            # self.configuration = pink.Configuration(self.robot.model, self.robot.data, q_out)
-            # pin.updateFramePlacements(self.robot.model, self.robot.data)
+            if self.limit_accel:
+                velocity = solve_ik(self.configuration, self.tasks.values(), self.dt, solver=self.solver, barriers=self.barriers, limits=self.limits, safety_break=False)
+                self.configuration.integrate_inplace(velocity, self.dt)
+                self.accellimit.set_last_integration(velocity, self.dt)
+            else:
+                velocity = solve_ik(self.configuration, self.tasks.values(), self.dt, solver=self.solver, barriers=self.barriers, safety_break=False)
+                self.configuration.integrate_inplace(velocity, self.dt)
 
             error = np.linalg.norm(self.T_target.translation - self.configuration.get_transform_frame_to_world(self.tasks["ee"].frame).translation)
             
@@ -184,19 +192,28 @@ class InverseKinematicsSolver:
                 self.update_robot_viz(self.configuration.q)
 
             if record:
-                # self.traj_qp.append(q_out)
-                self.traj_qp.append(self.configuration.q)
+                # self.traj_q.append(q_out)
+                self.traj_v.append(velocity)
+                self.traj_q.append(self.configuration.q)
 
             iterations += 1
             if iterations > self.max_iterations:
                 raise RuntimeError(f"IK did not converge within {self.max_iterations} steps. Current error: {error:.3e}")
+            
+            if np.all(velocity < self.velocity_thres):
+                if vel_below_thresh > 10:
+                    raise RuntimeError("All velocity values are below the threshold")
+                else:
+                    vel_below_thresh += 1
+
         sys.stdout.write("\nPosition reached\n")
 
         if x0:
             # self.q_x0 = q_out
-            # self.traj_qp.append(q_out)
+            # self.traj_q.append(q_out)
+            self.traj_v.append(velocity)
             self.q_x0 = self.configuration.q
-            self.traj_qp.append(self.configuration.q)
+            self.traj_q.append(self.configuration.q)
     
     def run_IK_to_x0(self):
         # First goal is the starting position(x0)
@@ -233,12 +250,13 @@ class InverseKinematicsSolver:
         # IK to goal
         self.IK_loop(record = True, x0 = False)
         
-        self.traj_qp = np.array(self.traj_qp) # Convert to np.array (only positions without velocity)
+        self.traj_q = np.array(self.traj_q) # Convert to np.array (only positions without velocity)
+        self.traj_v = np.array(self.traj_v)
 
-        sys.stdout.write("Trajectory of length: " + str(self.traj_qp.shape) + "\n")
+        sys.stdout.write("Trajectory of length: " + str(self.traj_q.shape) + "\n")
 
         self.pad_yref()
 
         self.config["mpc"]["x0_q"] = self.yref_pos_vel[0].tolist() # Add a new field for x0 in joint space
 
-        return self.yref_full, self.config
+        return self.yref_full, self.traj_v, self.config
