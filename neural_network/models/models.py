f21d64cb49bbc87b774f4bc72355329215ff7622
Nicodemus <N.sohyuji@student.tudelft.nl>
2026-02-28 14:00:14 +0100

    Revert TwoDofArm to 4 layer model.

--- neural_network/models/models.py
+++ neural_network/models/models.py
@@ -141,16 +141,14 @@ class TwoDofArmModel(nn.Module):                                            # Wi
         self.fc1 = nn.Linear(7, 64)
         self.fc2 = nn.Linear(64, 64)
         self.fc3 = nn.Linear(64, 64)
-        self.fc4 = nn.Linear(64, 64)
-        self.fc5 = nn.Linear(64, 1)
+        self.fc4 = nn.Linear(64, 1)
 
     def forward(self, x):
         x = self.fc0(x)                                                     # Linear transformation without activation ("scaling" layer)
         x = F.tanh(self.fc1(x))                                             # Hidden layers with tanh activations
         x = F.tanh(self.fc2(x))
         x = F.tanh(self.fc3(x))
-        x = F.tanh(self.fc4(x))
-        x = self.fc5(x)                                                     # Output layer without activation ("scaling" layer)
+        x = self.fc4(x)                                                     # Output layer without activation ("scaling" layer)
         x = torch.tensor(0.5, dtype=x.dtype, device=x.device) * x**2        # Least Squares which mimics acados cost
 
         return x
@@ -165,8 +163,7 @@ class TwoDofArmModelAcados(TwoDofArmModel):                                     
         x = F.tanh(self.fc1(x))                                             # Hidden layers with tanh activations
         x = F.tanh(self.fc2(x))
         x = F.tanh(self.fc3(x))
-        x = F.tanh(self.fc4(x))
-        x = self.fc5(x)                                                     # Output layer without activation ("scaling" layer)
+        x = self.fc4(x)                                                     # Output layer without activation ("scaling" layer)
         # x = torch.tensor(0.5, dtype=x.dtype, device=x.device) * x**2        # Least Squares which mimics acados cost
 
         return x
@@ -180,8 +177,8 @@ class iiwa14Model(nn.Module):                                            # Witho
     def __init__(self, train_config):
         super().__init__()
 
-        self.fc0 = ScaleLayer(15)
-        self.fc1 = nn.Linear(15, 64)
+        self.fc0 = ScaleLayer(17)
+        self.fc1 = nn.Linear(17, 64)
         self.fc2 = nn.Linear(64, 64)
         self.fc3 = nn.Linear(64, 64)
         self.fc4 = nn.Linear(64, 64)
@@ -199,16 +196,9 @@ class iiwa14Model(nn.Module):                                            # Witho
         return x
     
 @register_model
-class iiwa14ModelAcados(nn.Module):                                            # Without obstacles
+class iiwa14ModelAcados(iiwa14Model):                                            # Without obstacles
     def __init__(self, train_config):
-        super().__init__()
-
-        self.fc0 = ScaleLayer(15)
-        self.fc1 = nn.Linear(15, 64)
-        self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, 64)
-        self.fc4 = nn.Linear(64, 64)
-        self.fc5 = nn.Linear(64, 1)
+        super().__init__(train_config)
 
     def forward(self, x):
         x = self.fc0(x)                                                     # Linear transformation without activation ("scaling" layer)
