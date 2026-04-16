e76754773453838704202c26f729118615f0f317
Nicodemus <N.sohyuji@student.tudelft.nl>
2026-04-16 11:32:48 +0200

    Updated account

--- scripts/batch-VI.sh
+++ scripts/batch-VI.sh
@@ -2,13 +2,13 @@
 
 #SBATCH --job-name=VI_train
 #SBATCH --partition=compute
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
 #SBATCH --gpus-per-task=0
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=1024MB
-#SBATCH --account=education-me-msc-ro
+#SBATCH --account=research-me-cor
 
 module load 2025
 module load python
