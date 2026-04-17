21e91e95f638387fcbcf743c049c064252c2ed8c
Nicodemus <N.sohyuji@student.tudelft.nl>
2026-04-17 16:35:28 +0200

    different init for last layer

--- neural_network/models/models.py
+++ neural_network/models/models.py
@@ -25,14 +25,14 @@ class PendulumModel(nn.Module):
         self.fc1 = nn.Linear(2, 64)
         self.fc2 = nn.Linear(64, 64)
         self.fc3 = nn.Linear(64, 64)
-        self.fc4 = nn.Linear(64, 1)
+        self.fc_out = nn.Linear(64, 1)
 
     def forward(self, x):
         x = self.fc0(x)                                                     # Linear transformation without activation ("scaling" layer)
         x = F.tanh(self.fc1(x))                                             # Hidden layers with tanh activations
         x = F.tanh(self.fc2(x))
         x = F.tanh(self.fc3(x))
-        x = self.fc4(x)                                                     # Output layer without activation ("scaling" layer)
+        x = self.fc_out(x)                                                     # Output layer without activation ("scaling" layer)
         x = torch.tensor(0.5, dtype=x.dtype, device=x.device) * x**2        # Least Squares which mimics acados cost
 
         return x
@@ -141,14 +141,14 @@ class TwoDofArmModel(nn.Module):                                            # Wi
         self.fc1 = nn.Linear(7, 64)
         self.fc2 = nn.Linear(64, 64)
         self.fc3 = nn.Linear(64, 64)
-        self.fc4 = nn.Linear(64, 64)
+        self.fc_out = nn.Linear(64, 64)
 
     def forward(self, x):
         x = self.fc0(x)                                                     # Linear transformation without activation ("scaling" layer)
         x = F.tanh(self.fc1(x))                                             # Hidden layers with tanh activations
         x = F.tanh(self.fc2(x))
         x = F.tanh(self.fc3(x))
-        x = self.fc4(x)                                                     # Output layer without activation ("scaling" layer)
+        x = self.fc_out(x)                                                     # Output layer without activation ("scaling" layer)
         x = torch.tensor(0.5, dtype=x.dtype, device=x.device) * torch.sum(x**2, dim=1, keepdim=True)        # Least Squares which mimics acados cost
 
         return x
@@ -163,7 +163,7 @@ class TwoDofArmModelAcados(TwoDofArmModel):                                     
         x = F.tanh(self.fc1(x))                                             # Hidden layers with tanh activations
         x = F.tanh(self.fc2(x))
         x = F.tanh(self.fc3(x))
-        x = self.fc4(x)                                                     # Output layer without activation ("scaling" layer)
+        x = self.fc_out(x)                                                     # Output layer without activation ("scaling" layer)
 
         return x
     
@@ -181,7 +181,7 @@ class iiwa14Model(nn.Module):                                            # Witho
         self.fc2 = nn.Linear(64, 64)
         self.fc3 = nn.Linear(64, 64)
         self.fc4 = nn.Linear(64, 64)
-        self.fc5 = nn.Linear(64, 64)
+        self.fc_out = nn.Linear(64, 64)
 
     def forward(self, x):
         x = self.fc0(x)                                                     # Linear transformation without activation ("scaling" layer)
@@ -189,7 +189,7 @@ class iiwa14Model(nn.Module):                                            # Witho
         x = F.tanh(self.fc2(x))
         x = F.tanh(self.fc3(x))
         x = F.tanh(self.fc4(x))
-        x = self.fc5(x)                                                     # Output layer without activation ("scaling" layer)
+        x = self.fc_out(x)                                                     # Output layer without activation ("scaling" layer)
 
         x = torch.tensor(0.5, dtype=x.dtype, device=x.device) * torch.sum(x**2, dim=1, keepdim=True)        # Least Squares which mimics acados cost
 
@@ -206,6 +206,6 @@ class iiwa14ModelAcados(iiwa14Model):                                           
         x = F.tanh(self.fc2(x))
         x = F.tanh(self.fc3(x))
         x = F.tanh(self.fc4(x))
-        x = self.fc5(x)                                                     # Output layer without activation ("scaling" layer)
+        x = self.fc_out(x)                                                     # Output layer without activation ("scaling" layer)
 
         return x
